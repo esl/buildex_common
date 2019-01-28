284b8f68623c7ef2c4a6f1463285b1823139bd61
bryan <bryan.hunt@erlang-solutions.com>
2019-01-28 17:33:20 +0000

    travis config/coveralls

--- mix.exs
+++ mix.exs
@@ -4,18 +4,23 @@ defmodule Buildex.Common.MixProject do
   def project do
     [
       app: :buildex_common,
-      version: "0.1.0",
+      deps: deps(),
       elixir: "~> 1.7",
-      deps: deps()
+      test_coverage: [tool: ExCoveralls],
+      version: "0.1.0"
     ]
   end
 
   defp deps do
     [
-      {:poison, "~> 4.0"},
+      # {:credo, "~> 0.9.1", only: [:dev, :test]},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
       {:ex_docker_build, "~> 0.6.0"},
+      {:excoveralls, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:mimic, "~> 0.2", only: :test},
       {:mox, "~> 0.4", only: :test},
-      {:mimic, "~> 0.2", only: :test}
+      {:poison, "~> 4.0"}
     ]
   end
 end
